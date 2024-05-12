4cc51fabf8b790532e7094435fe54c9382cdf3f7
UfukDemrel <udemirel012@gmail.com>
2024-05-12 18:31:44 +0300

    maps 6

--- src/components/LocationModal.js
+++ src/components/LocationModal.js
@@ -82,7 +82,7 @@ const LocationModal = ({ closeModal, clickedLocation }) => {
       {clickedLocation && (
         <div>
           <img
-            className="mb-2 rounded-t-2xl"
+            className="rounded-t-2xl"
             src={clickedLocation.image2}
             alt="alt"
           />
@@ -159,15 +159,19 @@ const LocationModal = ({ closeModal, clickedLocation }) => {
                             </div>
                           )}
 
-                          <div className="flex justify-between items-center mt-3">
-                            <div className="font-semibold">{data.name}</div>
-                            <div className="font-semibold">100₺</div>
-                          </div>
+                          <div className="font-semibold text-center text-base">{data.name}</div>
 
-                          <div className="flex justify-between items-center mt-3 gap-3">
-                            <div class="quantity-control rounded-lg">
-                                <button class="quantity-btn">
-                                    <svg viewBox="0 0 409.6 409.6" fill="black">
+                          {data.size && (
+                            <div className="flex justify-between items-center mt-3 mb-3">
+                              {data.size.map((size) => (
+                                <div className="pl-3 pr-3 pt-1 pb-1 rounded-lg bg-slate-200 font-semibold text-sm" key={size.id}>{size.name}</div>
+                              ))}
+                            </div>
+                          )}
+
+                            <div class="flex justify-center rounded-lg mt-3 mb-3">
+                                <button class="border-2 border-slate-300 p-2 rounded-lg">
+                                    <svg viewBox="0 0 409.6 409.6" className="fill-slate-400" width="1rem" height="1rem">
                                         <g>
                                         <g>
                                             <path d="M392.533,187.733H17.067C7.641,187.733,0,195.374,0,204.8s7.641,17.067,17.067,17.067h375.467 c9.426,0,17.067-7.641,17.067-17.067S401.959,187.733,392.533,187.733z" />
@@ -175,14 +179,40 @@ const LocationModal = ({ closeModal, clickedLocation }) => {
                                         </g>
                                     </svg>
                                 </button>
-                                <input type="number" class="quantity-input" value="1" step="1" min="1" max="10" name="quantity"/>
-                                <button class="quantity-btn" >
-                                    <svg viewBox="0 0 426.66667 426.66667" fill="black">
+                                <input type="number" className="text-center" value="1" step="1" min="1" max="10" name="quantity"/>
+                                <button class="border-2 border-slate-300 p-2 rounded-lg">
+                                    <svg viewBox="0 0 426.66667 426.66667" className="fill-slate-400" width="1rem" height="1rem">
                                         <path d="m405.332031 192h-170.664062v-170.667969c0-11.773437-9.558594-21.332031-21.335938-21.332031-11.773437 0-21.332031 9.558594-21.332031 21.332031v170.667969h-170.667969c-11.773437 0-21.332031 9.558594-21.332031 21.332031 0 11.777344 9.558594 21.335938 21.332031 21.335938h170.667969v170.664062c0 11.777344 9.558594 21.335938 21.332031 21.335938 11.777344 0 21.335938-9.558594 21.335938-21.335938v-170.664062h170.664062c11.777344 0 21.335938-9.558594 21.335938-21.335938 0-11.773437-9.558594-21.332031-21.335938-21.332031zm0 0" />
                                     </svg>
                                 </button>
                             </div>
-                            <button class="btn btn5 rounded-lg">Next</button>
+
+                          <div className="flex justify-between items-center mt-3 mb-1">
+                            <div className="font-semibold">100TL</div>
+                            <div className="flex justify-between items-center gap-3 bg-black text-white p-2 rounded-lg">
+                              <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+                                width="1.3rem" height="1.3rem" viewBox="0 0 512.000000 512.000000"
+                                preserveAspectRatio="xMidYMid meet">
+
+                                <g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
+                                fill="white" stroke="none">
+                                <path d="M2393 4785 c-314 -57 -591 -276 -714 -566 -67 -157 -72 -193 -77
+                                -541 l-4 -318 -414 0 c-328 0 -421 -3 -447 -14 -42 -17 -85 -68 -92 -110 -8
+                                -42 269 -2383 291 -2457 48 -163 174 -314 324 -388 150 -74 98 -71 1300 -71
+                                1202 0 1150 -3 1300 71 150 74 276 225 324 388 22 74 299 2415 291 2457 -7 42
+                                -50 93 -92 110 -26 11 -119 14 -447 14 l-414 0 -4 318 c-5 348 -10 384 -77
+                                541 -98 230 -315 436 -548 521 -159 57 -342 74 -500 45z m302 -320 c238 -50
+                                439 -251 490 -491 11 -51 15 -141 15 -341 l0 -273 -640 0 -640 0 0 273 c0 315
+                                8 370 70 493 132 261 418 399 705 339z m-872 -1439 c103 -43 128 -177 48 -257
+                                -65 -65 -157 -65 -222 0 -124 123 13 325 174 257z m1600 0 c103 -43 128 -177
+                                48 -257 -112 -113 -296 -12 -267 146 18 94 128 150 219 111z"/>
+                                </g>
+                              </svg>
+                              <div className="text-sm">
+                                Shop Now
+                              </div>  
+                            </div>
+
                           </div>
                         </div>
                       ))}
