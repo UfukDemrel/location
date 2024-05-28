9ca8e297a0466a50591e6f1273389ba126fe7974
UfukDemrel <udemirel012@gmail.com>
2024-05-28 13:18:47 +0300

    maps-11

--- src/components/LocationModal.js
+++ src/components/LocationModal.js
@@ -309,7 +309,7 @@ const LocationModal = ({ closeModal, clickedLocation }) => {
                               name="quantity"
                             />
                             <button
-                              className={`border-2 border-black p-2 rounded-lg ${activeButton === "increase" ? "number" : ""}`}
+                              className={`border-2 border-black p-2 rounded-lg ${clickedLocation.color} ${activeButton === "increase" ? "number" : ""}`}
                               onClick={() => {
                                 increaseQuantity();
                                 handleButtonClick("increase");
